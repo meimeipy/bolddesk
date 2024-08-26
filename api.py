43515f7c221bfbf9643f090ae34d6af92b5105ae
meimeipy <avetaveeve@gmail.com>
2024-08-26 11:50:58 -0300

    dd

--- api.py
+++ api.py
@@ -554,7 +554,6 @@ def acharoccliente(dadoss):
             return f"Falha na solicitação: {str(e)}"
 
 from datetime import datetime
-import time
 
 def Abrir_Ticket(user_id, dadoss):
     dadoss = dict(dadoss)
@@ -775,18 +774,18 @@ def get_sender_name(conversationId, ticketId):
     headers = {
         "api_access_token": "8BNDLDVBN8nw4AmArzsHghZx"
     }
-    while True:
-        response = requests.get(url, headers=headers)
-        print("1get_sender_name", response.json())
-        if response.status_code == 200:
-            try:
-                data = response.json()
-                sender_name = data['last_non_activity_message']['sender']['name']
-                print("2get_sender_name", sender_name)
-                return sender_name
-            except KeyError:
-                pass
-        time.sleep(30)
+    response = requests.get(url, headers=headers)
+    print("1get_sender_name", response.json())
+    if response.status_code == 200:
+        try:
+            data = response.json()
+            sender_name = data['last_non_activity_message']['sender']['name']
+            print("2get_sender_name", sender_name)
+            return sender_name
+        except KeyError as e:
+            return f"Chave não encontrada: {str(e)}"
+        except json.JSONDecodeError as e:
+            return f"Erro ao decodificar JSON: {str(e)}"
 
 def editar_ticket(ticketId, sender_name):
     # URL para buscar detalhes dos agentes
