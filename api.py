85fe313082cfaff4d2f77affeeae6d870bf460e4
meimeipy <avetaveeve@gmail.com>
2024-08-26 14:08:14 -0300

    jjs

--- api.py
+++ api.py
@@ -678,7 +678,7 @@ def buscar_ticket_por_titulo_e_usuario(conversationId, user_id, assunto):
     time.sleep(60)
     try:
         logging.debug(f"Parâmetros recebidos: conversationId={conversationId}, user_id={user_id}, assunto={assunto}")
-        base_url = "https://vittle.bolddesk.com/api/v1/tickets"
+        base_url = "https://vittel.bolddesk.com/api/v1/tickets"
         headers = {
             "x-api-key": "1Ed7TGUUE0rzqjP5WCbsRZh56qtWP8eHHKXD9aK/+X0="
         }
