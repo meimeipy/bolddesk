94991271a892fc57fa5ea857f038f1d524f2b67d
meimeipy <avetaveeve@gmail.com>
2024-09-11 12:12:42 -0300

    SFD

--- api.py
+++ api.py
@@ -566,17 +566,11 @@ def Abrir_Ticket(user_id, dadoss):
     extracted_data = {key: dadoss.get(key) for key in ["Assunto", "Categoria", "Descrição"]}
     print("123", extracted_data)
     category_ids = {
-        "Telefonia IP": 11,
+        "VOZ IP": 11,
         "PABX IP": 12,
-        "Ramais Hardphone": 13,
-        "Ramais Softphone": 14,
-        "Ramais Agentes": 15,
         "Call Center": 16,
         "Sip Trunk": 17,
-        "Chat": 18,
-        "Omnichannel": 19,
-        "WhatsApp Web": 20,
-        "WhatsApp API Cloud": 21,
+        "Whatsapp/Chat": 18,
         "Cloud Server": 22,
         "SMS": 23
     }
@@ -919,21 +913,14 @@ def agenteachado(dados, user_id):
         # Assuming the response is a JSON object with a 'result' field containing a list
         results = response.json().get("result", [])
         category_ids = {
-                "Telefonia IP": 11,
+                "VOZ IP": 11,
                 "PABX IP": 12,
-                "Ramais Hardphone": 13,
-                "Ramais Softphone": 14,
-                "Ramais Agentes": 15,
                 "Call Center": 16,
                 "Sip Trunk": 17,
-                "Chat": 18,
-                "Omnichannel": 19,
-                "WhatsApp Web": 20,
-                "WhatsApp API Cloud": 21,
+                "Whatsapp/Chat": 18,
                 "Cloud Server": 22,
                 "SMS": 23
-            }
-            
+        }
         user_selection = dados.get('Categoriaa') or dados.get('Categoria')
         if results is None:
             return "Não existe tickets."
